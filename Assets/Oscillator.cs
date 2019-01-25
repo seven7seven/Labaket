efc3415f6cdee9a1a5abc37176c9f6d1128524bb
Catalin Pintea <catalin@crmrebs.com>
2019-01-25 14:59:17 +0200

    Added scale generation, synth now spits out notes in a key, using a pattern (e.g. D# minor)
    diff --git a/Assets/Oscillator.cs b/Assets/Oscillator.cs
    index 62e46b8..95e26bb 100644
    --- a/Assets/Oscillator.cs
    +++ b/Assets/Oscillator.cs
    @@ -12,15 +12,92 @@ public class Oscillator : MonoBehaviour
         public float gain;
         public float volume = 0.1f;
    
    +    static string[] tones = { "C", "C#", "D", "D#", "E", "F",
    +         "F#", "G", "G#", "A", "A#", "B" };
    +    static double[] frequencies = { 261.63, 277.18, 293.66, 311.13, 329.63, 349.23,
    +         369.99, 392.00, 415.30, 440.00, 466.16, 493.88 };
    +
    +    public static double[] current_scale;
    +    public static int scale_index;
    +
    +    static int FindTone(string key)
    +    {
    +        for (int i = 0; i < tones.GetLength(0); i++)
    +        {
    +            if (key == tones[i])
    +                return i;
    +        }
    +        return -1;
    +    }
    +
    +    static double GetFrequency(string key)
    +    {
    +        return frequencies[FindTone(key)];
    +    }
    +
    +    static double[] ComputeScale(string key, int[] steps)
    +    {
    +        double[] scale = new double[steps.Length];
    +
    +        int tonePosition = 0;
    +        int startTone;
    +
    +        startTone = FindTone(key);
    +
    +
    +        if (startTone < 0)
    +            return null;
    +        if (steps.GetLength(0) != scale.GetLength(0))
    +            return null;
    +
    +        tonePosition = startTone;
    +        for (int i = 0; i < steps.GetLength(0); i++)
    +        {
    +            scale[i] = frequencies[tonePosition % tones.GetLength(0)];
    +            tonePosition += steps[i];
    +        }
    +
    +        return scale;
    +    }
    +
    +    static void SetScale(double[] scale)
    +    {
    +        current_scale = scale;
    +        scale_index = 0;
    +    }
    +
    +    void Start()
    +    {
    +        Debug.Log("START");
    +
    +        int[] major = { 2, 2, 1, 2, 2, 2, 1 };
    +        int[] minor = { 2, 1, 2, 2, 1, 2, 2 };
    +
    +        int[] test = { 2, 2, 1, 2, 2, 2, 1, -1, -2, -2, -2, -1, -2, -2 };
    +        int[] test2 = { 2, 2, -1, -2, 2, 2, -5, -1 };
    +
    +        double[] scale = ComputeScale("A", test2);
    +
    +        Debug.Log(string.Join(", ", scale));
    +
    +        SetScale(scale);
    +    }
    +
         void Update()
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
    -            gain = 0;
    +            // Play next note in current scale
    +            gain = volume;
    +
    +            Debug.Log(current_scale);
    +
    +            frequency = current_scale[scale_index];
    +            scale_index = (scale_index + 1) % current_scale.Length;
             }
             if (Input.GetKeyUp(KeyCode.Space))
             {
    -            gain = volume;
    +            gain = 0;
             }
         }

--- Assets/Oscillator.cs
+++ Assets/Oscillator.cs
@@ -12,15 +12,92 @@ public class Oscillator : MonoBehaviour
     public float gain;
     public float volume = 0.1f;
 
+    static string[] tones = { "C", "C#", "D", "D#", "E", "F",
+         "F#", "G", "G#", "A", "A#", "B" };
+    static double[] frequencies = { 261.63, 277.18, 293.66, 311.13, 329.63, 349.23,
+         369.99, 392.00, 415.30, 440.00, 466.16, 493.88 };
+
+    public static double[] current_scale;
+    public static int scale_index;
+
+    static int FindTone(string key)
+    {
+        for (int i = 0; i < tones.GetLength(0); i++)
+        {
+            if (key == tones[i])
+                return i;
+        }
+        return -1;
+    }
+
+    static double GetFrequency(string key)
+    {
+        return frequencies[FindTone(key)];
+    }
+
+    static double[] ComputeScale(string key, int[] steps)
+    {
+        double[] scale = new double[steps.Length];
+
+        int tonePosition = 0;
+        int startTone;
+
+        startTone = FindTone(key);
+
+
+        if (startTone < 0)
+            return null;
+        if (steps.GetLength(0) != scale.GetLength(0))
+            return null;
+
+        tonePosition = startTone;
+        for (int i = 0; i < steps.GetLength(0); i++)
+        {
+            scale[i] = frequencies[tonePosition % tones.GetLength(0)];
+            tonePosition += steps[i];
+        }
+
+        return scale;
+    }
+
+    static void SetScale(double[] scale)
+    {
+        current_scale = scale;
+        scale_index = 0;
+    }
+
+    void Start()
+    {
+        Debug.Log("START");
+
+        int[] major = { 2, 2, 1, 2, 2, 2, 1 };
+        int[] minor = { 2, 1, 2, 2, 1, 2, 2 };
+
+        int[] test = { 2, 2, 1, 2, 2, 2, 1, -1, -2, -2, -2, -1, -2, -2 };
+        int[] test2 = { 2, 2, -1, -2, 2, 2, -5, -1 };
+
+        double[] scale = ComputeScale("A", test2);
+
+        Debug.Log(string.Join(", ", scale));
+
+        SetScale(scale);
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            gain = 0;
+            // Play next note in current scale
+            gain = volume;
+
+            Debug.Log(current_scale);
+
+            frequency = current_scale[scale_index];
+            scale_index = (scale_index + 1) % current_scale.Length;
         }
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            gain = volume;
+            gain = 0;
         }
     }
 
